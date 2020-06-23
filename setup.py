b32c01907fb94745a6b804982b3db6911356c21d
Bas <bas@shadowtrackr.com>
2020-06-23 15:13:56 +0200

    prepared for pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='shadowtrackr',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/shadowtrackr/python_API',
+    license='MIT',
+    author=' Bas van Schaik',
+    author_email='bas@shadowtrackr.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='A python API for ShadowTrackr',
+    install_requires=[
+         'requests'
+      ]
+)
