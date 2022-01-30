7b4859f842ae85508324ce8ece15c1b2d5edb095
Bas <GBPyQG2mrMcj>
2022-01-30 14:46:14 +0100

    more examples

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='shadowtrackr',
-    version='4.0',
+    version='4.1',
     packages=setuptools.find_packages(),
     url='https://github.com/shadowtrackr/python_API',
     license='MIT',
