a0a4a68988a42152eab5aa6ddf0ff120d96a2c7f
Bas <GBPyQG2mrMcj>
2022-01-30 14:36:23 +0100

    Add new query language examples

---
+++ api_example_usage.py
@@ -0,0 +1,23 @@
+from shadowtrackr import ShadowTrackr
+from pprint import pprint
+
+API_KEY = "ddb41951a4d2c61df93346fa9037f029"
+ADMINCODE = "87687bdb86ad1b1581ab39f9c198cbd4"
+
+st = ShadowTrackr(api_key=API_KEY)
+
+# More information about the Data Model and Query language::
+# https://test.shadowtrackr.com/docs/2-Data-Model
+# https://test.shadowtrackr.com/docs/3-Search-and-Queries
+
+certificate_issuers = st.query("index=certificates by issuer earliest=-10d")
+pprint(certificate_issuers)
+
+problem_hosts = st.query("index=hosts problem=yes earliest=-1m")
+pprint(problem_hosts)
+
+hosts_with_rdp_open = st.query("index=hosts ports=3389")
+pprint(hosts_with_rdp_open)
+
+all_spf_records = st.query("index=dns rrtype=txt rrdata=\"*spf*\"")
+pprint(all_spf_records)
